d9e75f7378839613a2c72a378846456141b906cb
Davin <30064993+D-Cach@users.noreply.github.com>
2025-05-16 05:31:17 -0700

    First commit

---
+++ interview_questions.py
@@ -0,0 +1,10 @@
+import math
+
+def compute_factorial(number):
+    return math.factorial(number)
+
+def test_compute_factorial():
+    input_number = 5
+    result = compute_factorial(input_number)
+    assert result == 120
+    print("Test Passed! The factorial of " + str(input_number) + " is " + str(result))
